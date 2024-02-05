6db810d4dd22825ae635c14b7ef6827b0d5697fb
ꜱōʜᴇʀᴜ ᴋāɴ (AKA) ꜱᴏʜᴀɪʟ ᴋʜᴀɴ <contact.sohail.me@gmail.com>
2024-02-05 18:23:06 +0000

    modified:   MeshRenameBot/translations/en.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -4,24 +4,24 @@ try:
     from .tconfig import Config
 except ImportError:
     class Config:
-        DATABASE_URL = [str, ""]
-        API_HASH = [str, "abcdedf......"]
-        API_ID = [int, 1234567]
-        BOT_TOKEN = [str, "bot:token here"]
+        DATABASE_URL = [str, "mongodb+srv://arthur:arthur0@cluster0.g5ihsk1.mongodb.net/?retryWrites=true&w=majority"]
+        API_HASH = [str, "a4476bdb326701fdadfe834593db59eb"]
+        API_ID = [int, 2663335]
+        BOT_TOKEN = [str, "6978317893:AAH4nprdiR7YSVD6n__wWXYRLAYyeRJXoLc"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
-        MAX_QUEUE_SIZE = [int, 5]
+        MAX_QUEUE_SIZE = [int, 100]
         SLEEP_SECS = [int, 10]
-        IS_MONGO = [bool, False]
+        IS_MONGO = [bool, True]
 
         # Access Restriction
         IS_PRIVATE = [bool, False]
-        AUTH_USERS = [list,[123456789]]
-        OWNER_ID = [int, 0]
+        AUTH_USERS = [list,[1922494807]]
+        OWNER_ID = [int, 953362604]
 
         # Public username url or invite link of private chat
-        FORCEJOIN = [str,""]
-        FORCEJOIN_ID = [int,-100123465978]
+        FORCEJOIN = [str,"https://t.me/HuntersOrgUpdates"]
+        FORCEJOIN_ID = [int,-1001804191007]
 
         TRACE_CHANNEL = [int, 0]
 
